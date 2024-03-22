ef86c7c35c4cf2f0fcab0fadabb2cdd27abac621
GrosJambon1243 <kevindurocher@live.ca>
2024-03-22 16:15:09 -0400

    Character Class

---
+++ AdventurerHallManager/AdventurerHallManager/Character.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+enum Race
+{
+    Elf,
+    Human,
+    Dwarf,
+    Tiefling,
+    Halfling,
+    HalfOrc,
+    Dragonborn,
+    Gnome,
+    HalfElf
+};
+
+enum Classe
+{
+    Fighter,
+    Cleric,
+    Rogue,
+    Wizard
+};
+
+
+class Character
+{
+public:
+    int damage;
+    int hp;
+    Race race;
+    Classe classe;
+    
+    Character(int _damage, int _hp, Race _race, Classe _classe, const string& _name, int _level)
+        : damage(_damage),
+          hp(_hp),
+          race(_race),
+          classe(_classe),
+          name(_name),
+          level(_level)
+    {}
+    ~Character();
+
+
+    
+private:
+    
+protected:
+    string name;
+    int level;
+    
+};
