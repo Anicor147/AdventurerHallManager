4f49b0f08bbcebfff9e1acf8d35003bae213ccce
GrosJambon1243 <kevindurocher@live.ca>
2024-03-27 21:41:37 -0400

    Main Menu Selection

--- AdventurerHallManager/AdventurerHallManager/AdventurerHallManager.cpp
+++ AdventurerHallManager/AdventurerHallManager/AdventurerHallManager.cpp
@@ -1,11 +1,47 @@
+
+
+#include <string>
+
 #include "Character.h"
 
 int main(int argc, char* argv[])
 {
+    bool inMainMenu = true;
+    string userChoice;
+    int userInt;
     Character* Bob = new Character(10, 50, Human, Fighter, "Bob", 1);
     Character Bob1 = Character(10, 50, Human, Fighter, "Bob1", 1);
 
-    cout << Bob->name << endl;
-    cout << Bob1.name << endl;
+   // cout << Bob->name << endl;
+    //cout << Bob1.name << endl;
+
+    // Main Menu
+    
+    std::cout << "Adventure Hall Manager\n1: Continue\n2: New Game\n3: Exit\n";
+    while (inMainMenu)
+    {
+        try
+        {
+            cin >> userChoice;
+            userInt = stoi(userChoice);
+            if (userInt == 1 || userInt == 2 || userInt == 3)
+            {
+                inMainMenu = false;
+                // Code suite au choix de l'utilisateur
+            }
+            else
+            {
+                throw(userChoice);
+            }
+            
+        }
+        catch (...)
+        {
+            std::cout << "Please choose a valid option\n";
+            
+        }
+    }
+    
+    
     return 0;
 }
